717f1fb6f0b8d324755c014440dcabb5d316fccd
Anna Komarova <anyavlasenko@gmail.com>
2026-07-02 19:55:16 +0200

    redirect

--- proxy.ts
+++ proxy.ts
@@ -1,26 +1,7 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
-import {
-  hasSiteAccess,
-  isSiteAccessPublicPath,
-  siteAccessAppliesToHost,
-} from "@/lib/site-access";
 
 export async function proxy(request: NextRequest) {
-  const host = request.nextUrl.hostname;
-  const { pathname } = request.nextUrl;
-
-  if (
-    siteAccessAppliesToHost(host) &&
-    !isSiteAccessPublicPath(pathname) &&
-    !hasSiteAccess(request)
-  ) {
-    const url = request.nextUrl.clone();
-    url.pathname = "/site-zugang";
-    url.searchParams.set("next", pathname + request.nextUrl.search);
-    return NextResponse.redirect(url);
-  }
-
   return updateSession(request);
 }
 
